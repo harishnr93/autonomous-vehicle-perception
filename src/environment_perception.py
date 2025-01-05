bbdb10383a29e82994429152146fad743082a34c
harishnr <harrish.nr@gmail.com>
2025-01-05 09:56:46 +0100

    av-perception

---
+++ src/environment_perception.py
@@ -0,0 +1,13 @@
+"""
+Date: 04.Dec.2024
+Author: Harish Natarajan Ravi
+Email: harrish.nr@gmail.com
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from m6bk import *
+from perception_algos import *
+
+np.random.seed(1)
---
+++ src/perception_algos.py
@@ -0,0 +1,10 @@
+"""
+Date: 04.Dec.2024
+Author: Harish Natarajan Ravi
+Email: harrish.nr@gmail.com
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from m6bk import *
